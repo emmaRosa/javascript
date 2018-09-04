fab5f1a4ead8ca44ae371124b7faaa9772f15a7c
Emma Rosa <41700446+emmaRosa@users.noreply.github.com>
2018-09-04 21:50:03 +0100

    Create vowelsAndConsonantLoop.js

---
+++ 10DaysofJS/vowelsAndConsonantLoop.js
@@ -0,0 +1,42 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function vowelsAndConsonants(s) {
+    var allvowels='aeiou'
+    var vowels='',consonants='';
+    for (var i of s) {
+        if (allvowels.indexOf(i) !== -1) {
+            vowels += i;
+        } else {
+            consonants += i;
+        }
+    }
+    var result = vowels + consonants;
+    for (i of result) console.log(i);
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
